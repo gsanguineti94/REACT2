0c9a556033f998696fd914c93de59e2ac105b04d
Guido Sanguineti <g.sanguineti94@gmail.com>
2022-08-14 13:13:09 -0300

    ItemList e Item, import de ItemList a Container

---
+++ src/components/Item/index.jsx
@@ -0,0 +1,15 @@
+import './item.css';
+
+import React from 'react';
+
+const Item = () => {
+    return(
+        <a href='' className="product">
+            <img src="https://i.snipboard.io/LX8jJq.jpg" alt="" />
+            <p>Focacchia</p>
+        </a>
+    )
+
+}
+
+export default Item;
--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,7 +1,13 @@
 import ItemCount from '../ItemCount';
+import ItemList from '../ItemList';
 import React from "react";
 import Title from '../title';
 
+const products = [
+    {id: 1, image: "https://imgur.com/fXA241T", prod: "Focacchia de tomate" },
+    {id: 2, image: "https://imgur.com/m7YamBC", prod: "Hogaza de masa madre"},
+    {id: 3, image: "https://imgur.com/IIs0te8", prod: "Pan de molde Brioche"},
+];
 
 export const ItemListContainer = ({ texto }) => {
 
@@ -14,6 +20,7 @@ export const ItemListContainer = ({ texto }) => {
         <>
             <Title greeting={texto} />
             <ItemCount initial={1} stock={5} onAdd={onAdd} />
+            <ItemList />
         </>
     );
 }
