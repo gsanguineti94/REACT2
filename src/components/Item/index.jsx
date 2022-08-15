ef202a6858f821b24b568b5e6e51f1995305bc6e
Guido Sanguineti <g.sanguineti94@gmail.com>
2022-08-15 19:14:12 -0300

    Me olvidé el timeout ahora está.

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 const Item = ({info}) => {
     return(
         <a href='' className="product">
-            <img src={info.image} alt="" />
-            <p>{info.prod}</p>
+            <img src={info.pictureUrl} alt="" />
+            <p>{info.title} ${info.price}</p>
         </a>
     )
 
