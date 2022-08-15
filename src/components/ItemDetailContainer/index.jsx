a209e5b3149b62d379d757dd2767d8518eaee484
Guido Sanguineti <g.sanguineti94@gmail.com>
2022-08-15 19:54:25 -0300

    Creacion archivos

---
+++ src/components/ItemDetailContainer/index.jsx
@@ -0,0 +1,10 @@
+import ItemDetail from "../ItemDetail";
+import React from "react";
+
+export const ItemDetailContainer = () => {
+    return(
+        <ItemDetail />
+    );
+}
+
+export default ItemDetailContainer;
