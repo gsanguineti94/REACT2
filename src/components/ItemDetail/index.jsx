0e7527eb760414c60a82961e3fdbf285d1238390
Guido Sanguineti <g.sanguineti@94gmail.com>
2022-08-22 21:36:43 -0300

    Mudanza itemcount a itemdetail y cambio de logica

--- src/components/ItemDetail/index.jsx
+++ src/components/ItemDetail/index.jsx
@@ -1,15 +1,32 @@
-import React from "react";
 import "./itemDetail.css"
 
-export const ItemDetail = ({data}) => {
-    return(
+import React, { useState } from "react";
+
+import ItemCount from '../ItemCount';
+import { Link } from "react-router-dom";
+
+
+
+export const ItemDetail = ({ data }) => {
+
+    const [gotoCart, setToCart] = useState(false)
+
+    const onAdd = (count) => {
+        setToCart(true);
+    }
+    return (
         <div className="container">
             <div className="detail">
-                <img className="detail_image"  src={data.pictureUrl} alt="" />
+                <img className="detail_image" src={data.pictureUrl} alt="" />
                 <div className="content">
                     <h1 className="title">{data.title}</h1>
                     <h1 className="price">{data.price}</h1>
                     <h2 className="description">{data.detail}</h2>
+                    {
+                        gotoCart
+                            ? <Link yo='/cart'>Terminar compra</Link>
+                            : <ItemCount initial={1} stock={5} onAdd={onAdd} />
+                    }
                 </div>
             </div>
         </div>
--- src/components/ItemListContainer/index.jsx
+++ src/components/ItemListContainer/index.jsx
@@ -1,24 +1,23 @@
-import ItemCount from '../ItemCount';
 import ItemList from '../ItemList';
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Title from '../title';
 import { useParams } from "react-router-dom";
 
 const products = [
-    {id: 1, pictureUrl: "https://i.imgur.com/fXA241T.jpg", title: "Focacchia de tomate", price: "$280", category:"Fochacchias"},
-    {id: 2, pictureUrl: "https://i.imgur.com/m7YamBC.jpg", title: "Hogaza de masa madre", price: "$500", category:"MasaMadre"},
-    {id: 3, pictureUrl: "https://i.imgur.com/IIs0te8.jpg", title: "Pan de molde Brioche", price: "$550", category:"Moldes"},
-    {id: 4, pictureUrl:"https://i.imgur.com/dm4n7EK.png", title:"Prepizza", price:"$250", category:"MasaMadre"},
-    {id: 5, pictureUrl:"https://i.imgur.com/zIJShhs.png", title:"Hogaza con semillas", price:"$600", category:"MasaMadre"},
-    {id: 6, pictureUrl:"https://i.imgur.com/pP59tNS.png", title:"Pan de Molde blanco", price:"$400", category:"Moldes"},
-    {id: 7, pictureUrl:"https://i.imgur.com/8TIi1nL.png", title:"Pan de Molde integral", price:"$500", category:"Moldes"},
+    { id: 1, pictureUrl: "https://i.imgur.com/mn8NsAM.png", title: "Focacchia de tomate", price: "$280", category: "Fochacchias" },
+    { id: 2, pictureUrl: "https://i.imgur.com/GrgoqfS.png", title: "Hogaza de masa madre", price: "$500", category: "MasaMadre" },
+    { id: 3, pictureUrl: "https://i.imgur.com/SKou6Rk.png", title: "Pan de molde Brioche", price: "$550", category: "Moldes" },
+    { id: 4, pictureUrl: "https://i.imgur.com/dm4n7EK.png", title: "Prepizza", price: "$250", category: "MasaMadre" },
+    { id: 5, pictureUrl: "https://i.imgur.com/zIJShhs.png", title: "Hogaza con semillas", price: "$600", category: "MasaMadre" },
+    { id: 6, pictureUrl: "https://i.imgur.com/pP59tNS.png", title: "Pan de Molde blanco", price: "$400", category: "Moldes" },
+    { id: 7, pictureUrl: "https://i.imgur.com/8TIi1nL.png", title: "Pan de Molde integral", price: "$500", category: "Moldes" },
 
 ];
 
 export const ItemListContainer = ({ texto }) => {
     const [data, setData] = useState([]);
 
-    const {categoriaId} = useParams();
+    const { categoriaId } = useParams();
 
     useEffect(() => {
         const getData = new Promise(resolve => {
@@ -29,20 +28,14 @@ export const ItemListContainer = ({ texto }) => {
         if (categoriaId) {
             getData.then(res => setData(res.filter(products => products.category === categoriaId)));
         }
-        else{
+        else {
             getData.then(res => setData(res));
         }
-    },[categoriaId]);
-
-    const onAdd = (count) => {
-        console.log('Compraste ' + count + ' unidades');
-        alert('Compraste ' + count + ' unidades');
-    }
+    }, [categoriaId]);
 
     return (
         <>
             <Title greeting={texto} />
-            <ItemCount initial={1} stock={5} onAdd={onAdd} />
             <ItemList data={data} />
         </>
     );
